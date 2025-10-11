4e4cf4fb3f85ecc5deb4f0243e736c9b80ad332f
Bharath <bharathdevaraju03@gmail.com>
2025-10-12 00:55:26 +0530

    Task 11

--- src/518-coin-changeII.cpp
+++ src/518-coin-changeII.cpp
@@ -4,7 +4,10 @@ using namespace std;
 
 class Solution {
    public:
-    int change(int amount, vector<int>& coins) {}
+    int change(int amount, vector<int>& coins) {
+        int numCoins = coins.size();
+        vector<vector<int>> dpTable(numCoins, amount);
+    }
 };
 
 int main() { return 0; }
